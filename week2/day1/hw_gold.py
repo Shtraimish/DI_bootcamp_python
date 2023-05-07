dd00f9b36978be6edab1c446e4593efb73adc66d
Shtraimish <shtraimish1@gmail.com>
2023-05-07 17:06:28 +0300

    homework_python_gold

---
+++ week2/day1/hw_gold.py
@@ -0,0 +1,10 @@
+#1
+print('hello world\n'*4 + 'i love python\n'*4 )
+
+#2
+month=int(input("please enter month number"))
+if 2> month >=5:print("this month is spring month")
+elif 5>month >=8:print("this month is summer")
+elif 8>month >=11 :print("this month is autumn")
+elif month ==12 or month < 3 :print("this month is winter")
+else:print("there is inly 12 month xD")
