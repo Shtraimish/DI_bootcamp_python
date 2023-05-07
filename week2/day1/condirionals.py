b1b9b8f81fb7f32b9c5ffc059ceb6a2c2452d3bb
Shtraimish <shtraimish1@gmail.com>
2023-05-07 15:03:33 +0300

    first commit

---
+++ week2/day1/condirionals.py
@@ -0,0 +1,7 @@
+bank_accoount=10000
+phone_price=2000
+computer_price=5000
+if bank_accoount>=computer_price:print("i can buy a computer")
+elif bank_accoount>=phone_price:print("i can buy a phone")
+else:
+    print("u don't have enough")    
---
+++ week2/day1/variables.py
@@ -0,0 +1,10 @@
+print(3 +2 )
+username= "John"
+email="jon@gmail.com"
+position_of_at=email.find("@")
+print(position_of_at)
+my_age=int(input("how old are u"))
+range_km=1.609*float(input("please, enter range in miles"))
+first_name=input("please enter ur name")
+last_name=input("please enter ur lastname")
+print(first_name,last_name)
