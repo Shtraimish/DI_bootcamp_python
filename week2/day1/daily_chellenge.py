3e813e3d2828b9cacaf66356d7804805b735cdaa
Shtraimish <shtraimish1@gmail.com>
2023-05-11 12:03:46 +0300

    daily challenge1

---
+++ week2/day1/daily_chellenge.py
@@ -0,0 +1,12 @@
+#1
+# password=input("please enter 10 symbols pasword")
+# if(len(password)<10):
+#     print("ur pass is too short")
+# elif(len(password)>10):
+#     print("ur pass is too long")
+# else:print("ur pass has correct length")
+
+#2
+# text=input("please enter text")
+# print("the first letter is ", text[0])
+# print("the last letter is", text[-1])
