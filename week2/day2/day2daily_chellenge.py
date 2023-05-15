8ff987ec4cc576fa543f9f9e577b1d1cacdff137
Shtraimish <shtraimish1@gmail.com>
2023-05-15 11:21:19 +0300

    day2DC

---
+++ week2/day2/day2daily_chellenge.py
@@ -0,0 +1,26 @@
+#1
+# number = int(input("Enter a number: "))
+# length = int(input("Enter the desired length: "))
+
+# multiples = []
+# count = 0
+# multiple = 0
+
+# while count < length:
+#     multiple += number
+#     multiples.append(multiple)
+#     count += 1
+
+# print("List of multiples:", multiples)
+#2
+# word=input("please enter the world")
+# letters= list(word)
+# modified_word=[]
+
+# count=0
+# for letter in letters:
+#     if letter in modified_word:
+#         print("second time letter:", letter)
+#     else: modified_word.append(letter)
+# final_word=''.join(modified_word)
+# print(final_word)
