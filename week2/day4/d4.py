aff479957f3d238d833df5dd07afba9edf37a006
Shtraimish <shtraimish1@gmail.com>
2023-05-18 14:22:04 +0300

    w1d4XP

---
+++ week2/day4/d4.py
@@ -0,0 +1,24 @@
+# l2 = [1, 2, 3, 4, 5, "a", "a", "b", 1, 2, 4]
+# s = l2.count(4)
+# print(s)
+
+# lst = [1, 2, 3, 4, 5, 6, 7]
+# print(lst[0:4])
+# print(lst[::])
+# print(lst[::-1])
+
+# set1 = {1, 2, 3, 4, "hi", "world", "python"}
+# print("python" in set1)
+# set1.remove(4)
+# print(set1)
+
+
+
+a = {1, 2, 3, 4, 5}
+b = {2, 3, 6, 7, 5}
+c = a^b 
+print(c)
+d = a - b
+print(d)
+e = b - a 
+print(e)
---
+++ week2/day4/d4_XP.py
@@ -0,0 +1,49 @@
+#1
+# # def display_message():
+#     print('i am learning python')
+
+# display_message()
+#2
+# book=input("please enter book name")
+# def favorite_book(book):
+    
+#     print('my favorite book is',book)
+# favorite_book(book)
+#3
+# def discribe_city(city,country):
+#     print(city,"is a city in",country)
+
+# discribe_city('kuiv','ukraine')
+#4
+# import random
+# def compare_numbers(num):
+#     random_num=random.randint(1,100)
+#     print(random_num)
+#     if num==random_num:
+#         print("numbers are equal")
+#     else:
+#         print("numbers are not equal")
+
+# compare_numbers(25)
+
+#5
+# def make_shirt(size,text):
+#     print("The size of the shirt is", size," and the text is", text)
+
+# make_shirt(45, "i love harry potter")
+# def make_shirt(size="large", message="I love Python"):
+#     print(f"The size of the shirt is {size} and the text is '{message}'")
+
+# make_shirt()
+
+#6
+#magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
+#def show_magicians(magicians):
+#     for magician in magicians:
+#         print(magician)
+#def the_great(magicians):
+#     for magician in magicians:
+#         print('the great',magician)
+
+#the_great(magician_names)
+#show_magicians(magician_names)
